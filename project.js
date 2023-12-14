b4fd7abbf0989f784e46c10fdfe0b2ba4c49e447
rrannah <106163823+rrannah@users.noreply.github.com>
2023-12-14 08:43:30 -0500

    Update project.js

--- project.js
+++ project.js
@@ -47,7 +47,7 @@ function scrollToTop() {
       behavior: "smooth" // Optional: Add smooth scrolling effect
     });
   }
-fetch('../database.json')
+fetch('https://raw.githubusercontent.com/rrannah/final-portfolio/27f7a842acd7bce20e72366903ffbad3292a6467/database.json')
     .then(response => response.json())
     .then(data => {
         // Loop through each project
